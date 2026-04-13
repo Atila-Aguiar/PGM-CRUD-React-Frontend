1c77481241ac2cb5a3029c666ab4cbca8ed2cef1
Atila_Aguiar <atilasilva007@gmail.com>
2026-04-13 15:38:36 -0300

    Adicionando rota do backend para subir no githubpages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://localhost:8000/api/tarefas";
+const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
   const [tarefas, setTarefas] = useState([]);
